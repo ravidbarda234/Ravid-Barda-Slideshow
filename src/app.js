9c89e2b20c9ed4ef947b0620c0b62745787d8eb8
ravidbarda <bardaravid@gmail.com>
2023-02-16 14:46:14 +0200

    router

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log();
+import PAGES from './/models/pageModel.js';
+import { onChangePage } from './routes/router.js';
+import { HOME_PAGE_LINK, ABOUT_PAGE_LINK } from './services/domservic.js';
+
+//האזנה לאירועים
+HOME_PAGE_LINK.addEventListener("click", () => onChangePage(PAGES.HOME));
+ABOUT_PAGE_LINK.addEventListener("click", () => onChangePage(PAGES.ABOUT));
---
+++ src/services/domservic.js
@@ -0,0 +1,8 @@
+/**** (pages) *****/
+export const HOME_PAGE = document.getElementById("home-page");
+export const ABOUT_PAGE = document.getElementById("about-page");
+
+
+/**** (pages links) *****/
+export const HOME_PAGE_LINK = document.getElementById("home-page-link");
+export const ABOUT_PAGE_LINK = document.getElementById("about-page-link");
